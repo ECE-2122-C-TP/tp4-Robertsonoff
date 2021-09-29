f626bac6b534cfe37b1599a9ea5c24a258545c6b
Théo Robert <91211230+Robertsonoff@users.noreply.github.com>
2021-09-29 16:48:22 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,13 +3,13 @@
 
 
 int main() {
-    //exercice1();
-    //exercice3();
-    //exercice4();
-    //exercice5();
-    //exercice7();
-    //exercice8();
-    //exercice9();
+    exercice1();
+    exercice3();
+    exercice4();
+    exercice5();
+    exercice7();
+    exercice8();
+    exercice9();
     exercice10();
 
     return 0;
